d8c764b516ae45b3e88a775f0f724783f59631ea
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-01-24 13:50:13 +0100

    Update contoller specs

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /users/:user_id/posts' do
-    before(:each) { get '/users/:user_id/posts' }
+    subject { User.new(name: 'Authur') }
+    before { subject.save }
+    before(:each) { get "/users/#{subject.id}/posts" }
 
     it 'Should return 200 status code' do
+      p subject.id
       expect(response).to have_http_status(200)
     end
 
@@ -18,17 +21,29 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'GET /users/:user_id/posts/:id' do
-    before(:each) { get '/users/:user_id/posts/:id' }
+    subject { User.new(name: 'Authur') }
+    before { subject.save }
 
     it 'Should return 200 status code' do
+      post = Post.new(author: subject, title: 'Hello', text: 'This is my first post')
+      get "/users/#{subject.id}/posts/#{post.id}"
+
       expect(response).to have_http_status(200)
     end
 
     it 'Should render the correct template' do
+      post = Post.new(author: subject, title: 'Hello', text: 'This is my first post')
+      post.save!
+      get "/users/#{subject.id}/posts/#{post.id}"
+
       expect(response).to render_template(:show)
     end
 
     it 'should render the correct text in the template' do
+      post = Post.new(author: subject, title: 'Hello', text: 'This is my first post')
+      post.save!
+      get "/users/#{subject.id}/posts/#{post.id}"
+
       expect(response.body).to include 'A user\'s full post with all comments'
     end
   end
