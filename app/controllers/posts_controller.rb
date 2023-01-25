ab1b793d648c405e9b1fdf084fb1c8e85aa12ec9
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-01-25 06:56:26 +0100

    Add the functionality to create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = Comment.where(post_id: params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    new_post = Post.new(author: current_user, title: params[:post][:title], text: params[:post][:text])
+    if new_post.save
+      redirect_to "/users/#{current_user.id}/posts"
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
 
-  # Post Routes
+  # Posts Routes
+  get '/post/new', to: 'posts#new'
+  post '/post/new', to: 'posts#create'
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
