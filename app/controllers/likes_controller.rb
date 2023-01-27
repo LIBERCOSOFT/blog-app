be6c32f931c1eeb13450fba57aa831259fafdf4b
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-01-27 16:58:29 +0100

    Implement best practices

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,9 @@
 class LikesController < ApplicationController
-  def create
+  def new
     post = Post.find(params[:post_id])
     new_like = Like.new(author: current_user, post:).update_likes_counter
     if new_like.save
-      redirect_to "/users/#{current_user.id}/posts", notice: 'Successfully Liked'
+      redirect_to user_posts_path
     else
       flash[:alert] = 'Post can\'t be liked at this time'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,26 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:new, :index, :show, :create] do
+      resources :comments, only: [:new, :create] 
+      resources :likes, only: [:new]
+    end
+  end
+
+  # get '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show'
 
   # Posts Routes
-  get '/post/new', to: 'posts#new'
-  post '/post/new', to: 'posts#create'
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
+  # get '/post/new', to: 'posts#new'
+  # post '/post/new', to: 'posts#create'
+  # get '/users/:user_id/posts', to: 'posts#index'
+  # get '/users/:user_id/posts/:id', to: 'posts#show'
 
   # Comments Routes
-  get '/post/:post_id/comment/new', to: 'comments#new'
-  post '/post/:post_id/comment/new', to: 'comments#create', as: 'new_comment'
+  # get '/post/:post_id/comment/new', to: 'comments#new'
+  # post '/post/:post_id/comment/new', to: 'comments#create', as: 'new_comment'
 
   # Like Routes
-  get '/post/:post_id/like/new', to: 'likes#create'
+  # get '/post/:post_id/like/new', to: 'likes#create'
 
 end
